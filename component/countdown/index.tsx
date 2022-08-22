9ac743e13383fbb2c0f34142c78875159b867609
ali-rajabinekoo <ali.rajabinekoo@protonmail.com>
2022-08-22 19:07:23 +0430

    Fix

---
+++ component/countdown/index.tsx
@@ -0,0 +1,20 @@
+import Countdown from "react-countdown";
+import {SetStateAction} from "react";
+import {CountdownTimeDeltaFn} from "react-countdown/dist/Countdown";
+
+const renderer = ({minutes, seconds, completed}: any) => {
+    return (
+        <span>ارسال مجدد کد تا {minutes}:{seconds} دقیقه دیگر</span>
+    );
+};
+
+class CountDownProps {
+    duration?: SetStateAction<any>
+    onComplete?: CountdownTimeDeltaFn | (() => void) | undefined
+}
+
+export const CountDown = ({duration, onComplete}: CountDownProps) => {
+    return (<span>
+        {!!duration && <Countdown date={duration} renderer={renderer} onComplete={onComplete}/>}
+    </span>)
+}
